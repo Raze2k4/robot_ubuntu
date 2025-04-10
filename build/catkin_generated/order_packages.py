052585e298f1d9a342d4d69a2dfd81bad5358237
Tran Thi <trandinhthi2908@gmail.com>
2025-04-10 15:10:20 +0700

    Initial commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ngoos/catkin_urdf/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ngoos/husky_ws/devel;/opt/ros/noetic'.split(';') if '/home/ngoos/husky_ws/devel;/opt/ros/noetic' != '' else []
---
+++ build/robile_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ngoos/catkin_urdf/src/robile_robot
+# Build directory: /home/ngoos/catkin_urdf/build/robile_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
