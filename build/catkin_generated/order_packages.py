7458be9d9b910d85f410d8ce63a6cefe02d5fe4e
Tran Thi <trandinhthi2908@gmail.com>
2025-05-15 18:09:23 +0700

    Initial commit

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/ngoos/catkin_urdf/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/ngoos/husky_ws/devel;/opt/ros/noetic'.split(';') if '/home/ngoos/husky_ws/devel;/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/ngoos/catkin_urdf/devel;/home/ngoos/husky_ws/devel;/opt/ros/noetic'.split(';') if '/home/ngoos/catkin_urdf/devel;/home/ngoos/husky_ws/devel;/opt/ros/noetic' != '' else []
